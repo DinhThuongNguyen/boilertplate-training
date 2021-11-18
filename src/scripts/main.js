4f3153797f485581db7b6f51a2deab6a8ecf3dd0
DinhThuongNguyen <dinhthuong092@gmail.com>
2021-11-18 08:27:56 +0700

    slice desktop feed

---
+++ src/scripts/main.js
@@ -0,0 +1,20 @@
+ $("#btn-one").on("click", function() {
+   $("#cls-one").toggleClass("dsk-show");
+ });
+ $("#cl-one").on("click", function() {
+   $("#cls-one").toggleClass("dsk-show");
+ });
+ 
+ $("#btn-two").on("click", function() {
+   $("#cls-two").toggleClass("dsk-show");
+ });
+ $("#cl-two").on("click", function() {
+   $("#cls-two").toggleClass("dsk-show");
+ });
+
+ $("#btn-three").on("click", function() {
+   $("#cls-three").toggleClass("dsk-show");
+ });
+ $("#cl-three").on("click", function() {
+   $("#cls-three").toggleClass("dsk-show");
+ });
